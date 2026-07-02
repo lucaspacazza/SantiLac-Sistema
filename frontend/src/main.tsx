c4bf4cdd74d2f0bfcac724c550ffc723c950c677
Lucas Pacazza <lucaspacazza6@gmail.com>
2026-07-02 10:49:29 -0300

    Ajustes de desempenho no front

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './styles.css'
-import './modules/qualidade/qualidade.css'
-import './modules/estoque/estoque.css'
-import './modules/pasteurizador/pasteurizador.css'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
